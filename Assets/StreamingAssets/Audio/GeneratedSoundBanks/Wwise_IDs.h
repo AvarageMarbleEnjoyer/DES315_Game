9985f045eeabfdffacc9a6fd7aca77a6ef94e5b7
AverageMarbleEnjoyer <2203180@uad.ac.uk>
2026-03-19 16:33:03 +0000

    Card UI events
    Added sound events for the card UI, such as for selecting cards.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,11 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID BLOODSTORM = 2566684358U;
+        static const AkUniqueID CARDUI_CONFIRM = 4049969704U;
+        static const AkUniqueID CARDUI_END = 982717551U;
+        static const AkUniqueID CARDUI_PICK = 2669983563U;
+        static const AkUniqueID CARDUI_SHUFFLE = 2654324891U;
+        static const AkUniqueID CARDUI_START = 2185107916U;
         static const AkUniqueID FIREFLINTLOCK = 1820443583U;
         static const AkUniqueID TORCHBREAK = 717429066U;
         static const AkUniqueID UI_CLOSE = 2519197294U;
