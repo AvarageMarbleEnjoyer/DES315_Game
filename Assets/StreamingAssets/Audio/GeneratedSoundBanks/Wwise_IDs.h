17c6c3bc879d8221d7de52d9be24984b6aeac37c
AverageMarbleEnjoyer <2203180@uad.ac.uk>
2026-03-19 17:41:11 +0000

    Wwise Vocal events
    Added vocal random container events to Wwise for the player and brute enemies.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,13 +14,22 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID BLOODSTORM = 2566684358U;
+        static const AkUniqueID BRUTEATTACK = 1580047315U;
+        static const AkUniqueID BRUTEBLOCK = 1416938370U;
+        static const AkUniqueID BRUTEHEAL = 1506802029U;
+        static const AkUniqueID BRUTEHURT = 1185467356U;
+        static const AkUniqueID BRUTEMOVE = 2186417968U;
         static const AkUniqueID CARDUI_CONFIRM = 4049969704U;
         static const AkUniqueID CARDUI_END = 982717551U;
         static const AkUniqueID CARDUI_PICK = 2669983563U;
         static const AkUniqueID CARDUI_SHUFFLE = 2654324891U;
         static const AkUniqueID CARDUI_START = 2185107916U;
         static const AkUniqueID FIREFLINTLOCK = 1820443583U;
+        static const AkUniqueID PLAYERDEATH = 1656947812U;
         static const AkUniqueID PLAYERHEAL = 3216143520U;
+        static const AkUniqueID PLAYERHURT = 3537581393U;
+        static const AkUniqueID PLAYERLAUGH = 2348858765U;
+        static const AkUniqueID PLAYERMELEEATTACK = 1598433574U;
         static const AkUniqueID TORCHBREAK = 717429066U;
         static const AkUniqueID UI_CLOSE = 2519197294U;
         static const AkUniqueID UI_NEXTPAGE = 1855554456U;
